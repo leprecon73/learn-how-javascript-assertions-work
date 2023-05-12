4f820d536481f39e05b36a6784e4df562157fc42
leprecon73 <leprecon73@gmail.com>
2023-05-12 22:55:11 +0400

    2.3

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -42,9 +42,9 @@ suite('Functional Tests', function () {
 
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.type, 'application/json');
-          assert.equal(res.body.name, 'Cristoforo');
-          assert.equal(res.body.surname, 'Colombo');
+          assert.equal(res.type, 'application/json', 'should be application/json');
+          assert.equal(res.body.name, 'Cristoforo', 'should be Cristoforo');
+          assert.equal(res.body.surname, 'Colombo', 'should be Colombo');
           done();
         });
     });
