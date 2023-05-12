20b5888cdfdfe6671f026ac4b78d9cf9862bba2f
leprecon73 <leprecon73@gmail.com>
2023-05-12 16:04:04 +0400

    1.8

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -63,8 +63,8 @@ suite('Unit Tests', function () {
     test('#isAbove, #isAtMost', function () {
       assert.isAtMost('hello'.length, 5);
       assert.isAbove(1, 0);
-      assert.isAtMost(Math.PI, 3);
-      assert.isAbove(1 - Math.random(), 1);
+      assert.isAbove(Math.PI, 3);
+      assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
     test('#isBelow, #isAtLeast', function () {
