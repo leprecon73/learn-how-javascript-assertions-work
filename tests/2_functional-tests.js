652b6565ec6944a450437e194ccbfe0d86f1ac3a
leprecon73 <leprecon73@gmail.com>
2023-05-13 12:17:07 +0400

    2.5test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -88,10 +88,17 @@ suite('Functional Tests with Zombie.js', function () {
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+    
+    test('Submit the surname "Polo" in the HTML form', function (done) {
+      browser.fill('surname', 'Polo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Marco');
+          browser.assert.text('span#surname', 'Polo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
@@ -101,3 +108,5 @@ suite('Functional Tests with Zombie.js', function () {
     });
   });
 });
+
+
